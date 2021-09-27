c780bae98eacb2b3561b2cea068fea143ecc2657
prathamesh <prathamesh19p@gmail.com>
2021-09-27 18:21:34 +0530

    api calls

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg"
-import notes from "../assets/data"
+// import notes from "../assets/data"
 
 function NotePage( {match} ) {
     let noteId = match.params.id
  
-    let note = notes.find(note => note.id == noteId)
+
+    let [note, setNote] = useState(null)
+
+    useEffect(() => {
+        
+        getNote()
+    }, [noteId])
+
+    let getNote = async () => {
+        let response = await fetch(`http://localhost:5000/notes/${noteId}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
+    // let note = notes.find(note => note.id == noteId)
+
     return(
         <div className="note">
             <div className="note-header">
